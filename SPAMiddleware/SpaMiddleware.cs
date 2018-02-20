2f5253bc572bd051fe22ea8207702aa939c6acc4
Dessel Bane <dessel.bane@gmx.de>
2018-02-20 10:07:47 +0100

    Added Index.html discovery in HostingEnv

--- SPAMiddleware/SpaMiddleware.cs
+++ SPAMiddleware/SpaMiddleware.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
 namespace SPAMiddleware
@@ -17,14 +19,30 @@ namespace SPAMiddleware
 
         #region Constructors
 
-        public SpaMiddleware(RequestDelegate next, SpaMiddlewareOptions options)
+        public SpaMiddleware(RequestDelegate next, SpaMiddlewareOptions options, IHostingEnvironment env)
         {
             _next = next;
-            _options = options;
+            _options = SetupPath(env, options);
         }
 
         #endregion
 
+        private static SpaMiddlewareOptions SetupPath(IHostingEnvironment env, SpaMiddlewareOptions options)
+        {
+            if (!string.IsNullOrWhiteSpace(options.PathToIndex))
+                if (File.Exists(options.PathToIndex))
+                    return options;
+
+            var indexFile = env.WebRootFileProvider
+                .GetDirectoryContents("")
+                .FirstOrDefault(x => x.Name.ToLower().Contains("index") && x.Name.ToLower().EndsWith("html"));
+            
+            if(indexFile != null)
+                return new SpaMiddlewareOptions(indexFile.PhysicalPath,options.SpecialRoutes);
+            
+            throw new ArgumentException("PathToIndex was incorrect.");
+        }
+        
         public async Task Invoke(HttpContext context)
         {
             if (!_options.SpecialRoutes.Any(x => context.Request.Path.StartsWithSegments(x)) && !Path.HasExtension(context.Request.Path.Value))
